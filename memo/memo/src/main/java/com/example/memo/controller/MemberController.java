866cb835d3f67e04e307c9eb689bff2b47e1719f
jinhyuk Jeong <117005839+wjdwlsgurdla@users.noreply.github.com>
2024-04-04 00:35:41 +0900

    FEAT: JWT 토큰
    토큰코드 생성

--- memo/memo/src/main/java/com/example/memo/controller/MemberController.java
+++ memo/memo/src/main/java/com/example/memo/controller/MemberController.java
@@ -1,17 +1,21 @@
 package com.example.memo.controller;
 
 import com.example.memo.dto.MemberDTO;
+import com.example.memo.service.JwtService;
 import com.example.memo.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @Controller
 @RequiredArgsConstructor//MemberService에 대한 멤버 사용 가능
 public class MemberController {
     //생성자 주입
     private final MemberService memberService;
+    private final JwtService jwtService;
     //회원가입 페이지 출력 요청
     @GetMapping("/member/save") //signup
     public String saveForm(){
@@ -31,12 +35,14 @@ public class MemberController {
         return "login";
     }
     @PostMapping("/member/login")//session: 로그인 유지
-    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+    @CrossOrigin("*")
+    public String login(@RequestBody MemberDTO memberDTO, HttpSession session){
         MemberDTO loginResult=memberService.login(memberDTO);
         if (loginResult != null) {//login 성공
-            session.setAttribute("loginEmail",loginResult.getMemberEmail());
-//            session.setAttribute("loginName",loginResult.getMemberName());
-            return "main";
+            // 로그인 성공 시 JWT 토큰 발급
+            String jwtToken = jwtService.createJwt(loginResult.getUid());
+            // JWT 토큰을 클라이언트에게 전달
+            return jwtToken;
         }else{//login 실패
             return "login";
         }
