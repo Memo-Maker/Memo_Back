c160b3dcbb3892a5f71349c0a38ad8d23a2a77d2
yeongseo <115892001+Cyeongseo@users.noreply.github.com>
2024-04-30 15:55:16 +0900

    FEAT: category 삭제 기능 추가
    ## 작업 내용
    category 삭제 기능 추가

--- memo/memo/src/main/java/com/example/memo/controller/CategoryController.java
+++ memo/memo/src/main/java/com/example/memo/controller/CategoryController.java
@@ -14,11 +14,19 @@ public class CategoryController {
 
     @Autowired
     public CategoryController(CategoryService categoryService){this.categoryService=categoryService;}
-
+    //category 저장
     @PostMapping
     public ResponseEntity<Void> addCategory(@RequestBody CategoryDto categoryDto) {
         categoryService.addCategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
+    //category 삭제
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable("categoryId") long categoryId) {
+        if (categoryService.deleteCategory(categoryId)) {
+            return ResponseEntity.ok().build(); // 성공적으로 삭제되면 200 OK 응답
+        } else {
+            return ResponseEntity.notFound().build(); // 해당 ID의 카테고리가 없는 경우 404 Not Found 응답
+        }
+    }
 }
